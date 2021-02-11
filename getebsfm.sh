cf922a9b62857ff88db7387dae19f2d5200957c5
chitack <chitack.chang@gmail.com>
2021-02-11 16:42:13 +0900

    초급중국어는 끄기 제외항목으로 추가

--- getebsfm.sh
+++ getebsfm.sh
@@ -71,7 +71,7 @@ else
 fi
 find $HOME -maxdepth 1 -type f -mtime +50 -name "$PROGRAM_NAME*" -exec rm -rf {} \;
 RECDATEONLY=`date +%Y%m%d` 
-TOTALMP3SIZE=`du -chb  *"$RECDATEONLY"* | grep total | awk '{print $1}'`
+TOTALMP3SIZE=`du -chb --exclude=*Chinese*.mp3 *"$RECDATEONLY"* | grep total | awk '{print $1}'`
 echo $TOTALMP3SIZE
 if (( $TOTALMP3SIZE >= 37000000 )); then
         echo "enough size, turn off"
